f7010cb8d8fd8919804f39d2293a3cc74ba3a869
Sebastian Wuchterl <sebastian.wuchterl@cern.ch>
2021-11-17 17:58:13 +0100

    update default

--- HLTrigger/Configuration/python/Run3/test_All.py
+++ HLTrigger/Configuration/python/Run3/test_All.py
@@ -10,21 +10,15 @@ process = TRK_newTracking(process) # New Tracking (patatrack tracks + single ite
 #######process = MUO_newIO(process) ## New ML-based inside-out seeding for muon reconstruction #Conflict with #MUO_newTracking
 #process = MUO_newOI(process) ## New ML-based outside-in muon for muon reconstruction
 
-#process = BTV_roiCalo_roiPF_DeepCSV(process) ## Calo b-tagging: new regional calo b-tagging [new sequence] ## PF b-tagging: new regional DeepCSV PF b-tagging [new sequence] 
-#process = BTV_roiCalo_globalPF_DeepCSV(process) ## Calo b-tagging: new regional calo b-tagging [new sequence] ## PF b-tagging: new global DeepCSV PF b-tagging 
+# process = BTV_noCalo_roiPF_DeepCSV(process) ## Calo b-tagging: none ## PF b-tagging: new regional DeepCSV PF b-tagging [new sequence]
+# process = BTV_roiCalo_roiPF_DeepCSV(process) ## Calo b-tagging: new regional calo b-tagging [new sequence] ## PF b-tagging: new regional DeepCSV PF b-tagging [new sequence]
+#process = BTV_roiCalo_globalPF_DeepCSV(process) ## Calo b-tagging: new regional calo b-tagging [new sequence] ## PF b-tagging: new global DeepCSV PF b-tagging
+#process = BTV_globalCalo_globalPF_DeepCSV(process) ## Calo b-tagging: new global calo b-tagging [new sequence] ## PF b-tagging: new global DeepCSV PF b-tagging
 
-#process = BTV_roiCalo_roiPF_DeepJet(process) ## Calo b-tagging: new regional calo b-tagging [new sequence] ## PF b-tagging: new regional DeepJet PF b-tagging [new sequence] 
-process = BTV_roiCalo_globalPF_DeepJet(process) ## Calo b-tagging: new regional calo b-tagging [new sequence] ## PF b-tagging: new global DeepJet PF b-tagging 
-
-#process = BTV_noCalo_roiPF(process) ## Calo b-tagging: none ## PF b-tagging: new regional PF b-tagging [new sequence]
-#process = BTV_globalCalo_globalPF(process) ## Calo b-tagging: new "global" calo b-tagging ## PF b-tagging: new global PF b-tagging
-
-#process = BTV_addMCDeepJetPath(process) ## Add MC_PFBTagDeepJet
-#process = BTV_addMCDeepJetROIForBTagPath(process) ## Add MC_PFBTagDeepJetROIForBTag
-
-#process = BTV_moveToDeepJet(process) ##Add a DeepJet version to all the paths with PF b-tagging
-#process = BTV_moveToDeepJetROI(process)  ##Add a DeepJet version based on ROI PF to all the paths with PF b-tagging
-
+#process = BTV_noCalo_roiPF_DeepJet(process) ## Calo b-tagging: none ## PF b-tagging: new regional DeepJet PF b-tagging [new sequence]
+# process = BTV_roiCalo_roiPF_DeepJet(process) ## Calo b-tagging: new regional calo b-tagging [new sequence] ## PF b-tagging: new regional DeepJet PF b-tagging [new sequence]
+# process = BTV_roiCalo_globalPF_DeepJet(process) ## Calo b-tagging: new regional calo b-tagging [new sequence] ## PF b-tagging: new global DeepJet PF b-tagging
+process = BTV_globalCalo_globalPF_DeepJet(process) ## Calo b-tagging: new global calo b-tagging [new sequence] ## PF b-tagging: new global DeepJet PF b-tagging
 
 ### Drop EndPaths ###
 els = process.__dict__
@@ -32,4 +26,3 @@ for el in list(els):
     if  (type(els[el]) == cms.OutputModule) or (type(els[el]) == cms.EndPath) or el == "PrescaleService" or el == "datasets" or el == "streams":
         print("Deleting %s (%s)"%(el, type(els[el])))
         delattr(process, el)
-
